b074d08cbd2a21921d9316358500710377c03006
NohGunho <gunho0210@naver.com>
2020-03-13 13:40:11 +0900

    follow기능 커밋 (getStream.io follow 제외)

--- backend/src/controllers/FollowController.js
+++ backend/src/controllers/FollowController.js
@@ -1,11 +1,8 @@
-const Post = require("../models/Follow"),
+const Follow = require("../models/Follow"),
   stream = require("getstream");
 
 module.exports = {
-
-  async store(req, res) {
-    const { follower, followId, followGroup, followCode } = req.body;
-
+  async follow(req, res) {
     // getStream.io Dashboard Connect
     var streamClient = stream.connect(
       "p5mv3rqjj4u6",
@@ -13,21 +10,44 @@ module.exports = {
       "70719"
     );
 
+    console.log(req.params.follower);
+    console.log(req.params.sectionId);
+
     // timeline feed setting
-    var timelineFeed = streamClient.feed("timeline", `${follower}`);
+    var timelineFeed = streamClient.feed("timeline", `${req.params.follower}`);
 
-    // follow add
-    timelineFeed.follow("user", `${followId}`);
+    // 구독한 리스트가 있는지 db에서 데이터 확인.
+    const followList = await Follow.find({ follower: req.params.follower });
+    
+    // 첫번째 구독이라면 follow model 을 생성하여 DB에 데이터 최초 저장.
+    if (followList.length === 0) {
+      let follow = new Follow({
+        follower: req.params.follower,
+        sectionList: [
+          {
+            sectionId: req.params.sectionId,
+            imgUrl: "test.jpg"
+          }
+        ]
+      });
 
-    // Follow Collection add
-    const follow = await Follow.create({
-      followId,
-      followGroup,
-      followCode,
-      follower
-    });
-    req.io.emit("follow", follow);
+      follow.save(function(err, list) {
+        if (err) return console.error(err);
+        console.log("subscribe add success!");
+        //timelineFeed.follow("user", `${req.query.sectionId}`);  db insert success 시 getStream.io follow 추가
+      });
+    } else { // 두번째 이상 구독이라면 기존에 저장되었던 follow 테이블에 데이터를 가져와 신규로 구독한 데이터를 push.
+      followList[0].sectionList.push({
+        sectionId: req.params.sectionId,
+        imgUrl: "test1.jpg"
+      });
 
-    return res.json(follow);
+      console.log(followList[0].sectionList);
+      followList[0].save(function(err, list) {
+        if (err) return console.error(err);
+        console.log("subscribe add success!");
+         //timelineFeed.follow("user", `${req.query.sectionId}`);  db insert success 시 getStream.io follow 추가
+      });
+    }
   }
 };
