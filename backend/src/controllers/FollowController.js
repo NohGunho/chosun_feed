56bd13aa04c8af37a39c79d5ddc6c6b2c31a1069
NohGunho <gunho0210@naver.com>
2020-03-27 15:38:46 +0900

    follow, unfollow 기능 getStream.io 기반 변경 완료

--- backend/src/controllers/FollowController.js
+++ backend/src/controllers/FollowController.js
@@ -1,32 +1,42 @@
 const Follow = require("../models/Follow"),
-  stream = require("getstream");
+  stream = require("getstream")
+  , config = require("../config/config");
+
+// follow 와 unfollow , 자기 자신의 구독리스트를 가져오는 controller
 
 module.exports = {
   // 내 구독 리스트
   async index(req, res) {
-    const followList = await Follow.find(
-      { follower: `${req.params.follower}` },
-      { "sectionList.sectionId": 1 }
-    );
+
+    let feedList = [];
+    
+    var timelineFeed = config.GETSTREAM.feed("timeline", "test");
+
+    function activitiesSuccess(successData) {
+      feedList = successData.results;
+    }
+
+    function activitiesError(errorData) {
+      console.log(errorData);
+    }
+    // getStream.io에서 follow 리스트 가져오기
+    await timelineFeed.following({limit: '10'}).then(activitiesSuccess).catch(activitiesError);
+
     // 구독한 섹션리스트 json 데이터에서 배열로 만들기.
     const sectionArray = [];
-    if (followList.length !== 0) {
-      for (var i = 0; i < followList[0].sectionList.length; i++) {
-        sectionArray[i] = followList[0].sectionList[i].sectionId;
+    if (feedList.length !== 0) {
+      for (var i = 0; i < feedList.length; i++) {
+        sectionArray[i] = (feedList[i].target_id).split(":")[1];
       }
     }
+
+    console.log(sectionArray);
 
     return res.json(sectionArray);
   },
 
   //follow 기능
   async follow(req, res) {
-    // getStream.io Dashboard Connect
-    var streamClient = stream.connect(
-      "p5mv3rqjj4u6",
-      "qbanwcyuyvts8s48vtbhphc645zbv7fzudvdp6wvjxjbd77msunquxf2z7hzw2te",
-      "70719"
-    );
 
     // id , section , imgUrl 값 세팅
     const follower = req.params.follower;
@@ -34,7 +44,7 @@ module.exports = {
     const imgUrl = "test.jpg";
 
     // timeline feed setting
-    var timelineFeed = streamClient.feed("timeline", `${follower}`);
+    var timelineFeed = config.GETSTREAM.feed("timeline", `${follower}`);
 
     // 구독리스트가 있는지 db에서 데이터 확인.
     const followList = await Follow.find({ follower: follower });
@@ -61,7 +71,12 @@ module.exports = {
         if (err) return console.error(err);
         console.log("subscribe add success!");
         result = "success";
-        //timelineFeed.follow("user", `${req.query.sectionId}`);  db insert success 시 getStream.io follow 추가
+        //db insert success 시 getStream.io follow 추가
+        timelineFeed.follow("user", sectionId).then(
+          result = "sucess"
+        ).catch(
+          result = "error"
+        );  
       });
     } else if (sectionList.length !== 1) {
       // 두번째 이상 구독이라면 기존에 저장되었던 follow 테이블에 데이터를 가져와 신규로 구독한 데이터를 push.
@@ -70,12 +85,17 @@ module.exports = {
         imgUrl: "test1.jpg"
       });
 
-      console.log(followList[0].sectionList);
+      // DB save가 성공하면 getStream.io 에도 데이터 저장
       followList[0].save(function(err, list) {
         if (err) return console.error(err);
         console.log("subscribe add success!");
         result = "success";
-        //timelineFeed.follow("user", `${req.query.sectionId}`);  db insert success 시 getStream.io follow 추가
+        //db insert success 시 getStream.io follow 추가
+        timelineFeed.follow("user", sectionId).then(
+          result = "sucess"
+        ).catch(
+          result = "error"
+        );
       });
     }
 
@@ -84,24 +104,43 @@ module.exports = {
 
   //unfollow기능
   async unfollow(req, res) {
+
+    //response 값
+    let result = "";
+
+    // 고객id , unfollow할 sectionId 가져오기
     var follower = req.params.follower;
     const sectionId = req.params.sectionId;
 
+    // DB에서 삭제할 데이터 찾기.
     const followList = await Follow.find({ follower: follower });
-    const sectionList = await Follow.find({ follower: follower },{"sectionList._id":1,"sectionList.sectionId":1})
-      .where("sectionList.sectionId")
-      .equals(sectionId);
+    const sectionList = await Follow.find({ follower: follower },{_id:0,follower:0, sectionList:{$elemMatch:{sectionId:sectionId}}});
     
-    console.log(followList);
-    console.log(sectionList[0].sectionList[0]);
+    // 삭제할 배열의 _id 값 가져오기
+    const sectionIdx = sectionList[0].sectionList[0]._id;
 
-    if (followList.lenth !== 0) {
-      followList[0].sectionList.remove({sectionId : sectionId }); 
+    //타임 라인 세팅
+    var timelineFeed = config.GETSTREAM.feed("timeline", `${follower}`);
 
+    if (followList.lenth !== 0 && sectionList[0].sectionList.length !==0 ) {
+      // DB에서 데이터 삭제
+      followList[0].sectionList.remove({_id : sectionIdx });
+      
+      // DB save가 성공하면 getStream.io 에도 데이터 삭제
       followList[0].save(function(err, list) {
         if (err) return console.error(err);
         console.log("subscribe remove success!");
+        // getStream.io 데이터 삭제
+        timelineFeed.unfollow("user", sectionId).then(
+          result = "sucess"
+        ).catch(
+          result = "error"
+        );
       });
+    }else{
+      result = "error";
     }
+
+    return res.json(result);
   }
 };
--- backend/src/controllers/SubscribeController.js
+++ backend/src/controllers/SubscribeController.js
@@ -1,6 +1,8 @@
 const Subscribe = require("../models/Subscribe"),
 Post = require('../models/Post');
 
+// 구독 추천 페이지를 위한 섹션별 , 기자별 구독할 수 있는 전체 리스트를 가져오기 위한 controller
+
 module.exports = {
 
   async index(req, res) {
